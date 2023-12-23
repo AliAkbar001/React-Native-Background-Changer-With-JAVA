6900638ce926b4472c1ca9080307698325e27c52
DESKTOP-5T0E1CO\AliAkbar <786aliakbar.aa@gmail.com>
2023-12-23 20:28:05 +0500

    Create Home Page

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,14 +1,50 @@
-import { View, Text, Button } from 'react-native'
+import { Text, Image, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { gallery } from '../galleryJSON'
 
 export default function Home({ navigation }) {
+
+  function handleNavigation(image:string){
+    navigation.navigate('SetWallpaper');
+  }
+  const listItems = ({item})=>(
+    <TouchableOpacity  style={styles.imageBox} onPress={() => handleNavigation(item.path)}>
+      <Image source={item.path} style={styles.image}/> 
+      <Text style={styles.title}>{item.name}</Text>
+    </TouchableOpacity>
+  )
+
   return (
-    <View>
-      <Text>Home</Text>
-      <Button
-        title="Set Wallpaper"
-        onPress={() => navigation.navigate('SetWallpaper')}
-      />
-    </View>
+    <FlatList
+      data={gallery}
+      renderItem={listItems}
+      keyExtractor={(item) => item.name}
+      numColumns={2}
+    />
+    // <ScrollView>
+    //   <View style={styles.container}>
+    //     { gallery.map((value, key:number) => 
+    //         <View key={key} style={styles.imageBox}>
+    //           <Image source={value.path} style={styles.image}/> 
+    //           <Text style={styles.title}>{value.name}</Text>
+    //         </View>
+    //     )}
+    //   </View>
+    // </ScrollView>
   )
 }
+const styles = StyleSheet.create({
+  imageBox:{
+    width:'44%',
+    justifyContent:'center',
+    alignItems: 'center',
+    margin:10,
+  },image:{
+    width: '100%',
+    height:300,
+  },title:{
+    color: 'black',
+    fontWeight: 'bold',
+    letterSpacing: 1
+  }
+})
---
+++ galleryJSON.js
@@ -0,0 +1,21 @@
+export const gallery = [
+    {
+        name: 'Foggy Trees',
+        path: require('./assets/photo-1.jpeg')
+    },{
+        name: 'Black Butterfly',
+        path: require('./assets/photo-2.jpeg')
+    },{
+        name: 'Aesthetic Street',
+        path: require('./assets/photo-3.jpeg')
+    },{
+        name: 'Red Roses',
+        path: require('./assets/photo-4.jpeg')
+    },{
+        name: 'White Roses',
+        path: require('./assets/photo-5.jpeg')
+    },{
+        name: 'Pink Sky',
+        path: require('./assets/photo-6.jpeg')
+    }
+]
