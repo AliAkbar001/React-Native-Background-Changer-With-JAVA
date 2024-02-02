469c8baa4bf7d2e280e5705c75933f4f4c917f06
AliAkbar001 <786aliakbar.aa@gmail.com>
2024-02-02 12:30:27 +0500

    Changes Test

--- galleryJSON.js
+++ galleryJSON.js
@@ -6,7 +6,7 @@ export const gallery = [
         name: 'Black Butterfly',
         path: 'https://images.unsplash.com/photo-1702906220516-11f24e4423c2?q=80&w=3774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },{
-        name: 'Aesthetic Street',
+        name: 'Aesthetic Street Pic',
         path: 'https://images.unsplash.com/photo-1533396371595-d46b0aa39bd2?q=80&w=2851&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },{
         name: 'Red Roses',
